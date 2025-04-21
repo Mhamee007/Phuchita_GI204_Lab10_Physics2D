7f6cd831f34bf17c318fccf4223cd8dda3fe6c5f
Mhamee007 <Mhameework@gmail.com>
2025-04-21 16:44:33 +0700

    1.2

---
+++ Assets/Scenes/playerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody2D r2d;
+    float move;
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        r2d.velocity = new Vector2 (move * speed, r2d.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            r2d.AddForce(new Vector2(r2d.velocity.x, jumpForce));
+        }
+    }
+}
